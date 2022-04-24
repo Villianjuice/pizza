d034c8517a57d12c3ac2d35b377dd4ecdf86fbf2
Aslan <tavorktar@gmail.com>
2022-04-24 15:06:45 +0600

    set up navigation

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -1,17 +1,21 @@
 import React, { useState } from 'react';
 
-const Categories = ({items}) => {
-	const [activeItem, setActiveItem] = useState(null)
-	const handleActiveItem = (item) => {
-		setActiveItem(item)
-	}
+const Categories = ({ items, onClickItem }) => {
+  const [activeItem, setActiveItem] = useState(null);
+  const selectItem = (item) => {
+    setActiveItem(item);
+  };
   return (
     <ul>
-      <li className={`${activeItem === null ? 'active' : ''}`}>Все</li>
+      <li
+        onClick={() => selectItem(null)}
+        className={`${activeItem === null ? 'active' : ''}`}>
+        Все
+      </li>
       {items.map((name, index) => (
         <li
           key={`${name}_${index}`}
-          onClick={() => handleActiveItem(index)}
+          onClick={() => selectItem(index)}
           className={`${activeItem === index ? 'active' : ''}`}>
           {name}
         </li>
