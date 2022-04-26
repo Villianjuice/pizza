8456a7f42590f0bafb24f2d6bee732c606265636
Aslan <tavorktar@gmail.com>
2022-04-26 13:08:58 +0600

    add typecheck

--- src/App.js
+++ src/App.js
@@ -1,19 +1,24 @@
-import React from 'react';
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
 import { Route, Routes } from 'react-router-dom';
-import { Header } from './components'
+import { Header } from './components';
 import { Cart, Home } from './pages';
-import './scss/app.scss'
-
+import './scss/app.scss';
 
 function App() {
+  const [pizzas, setPizzas] = useState([]);
+
+  useEffect(() => {
+    axios.get('http://localhost:3000/db.json').then(({data}) => setPizzas(data.pizzas))
+  }, []);
   return (
     <div className="App">
       <div className="wrapper">
-        <Header/>
+        <Header />
         <div className="content">
           <Routes>
-            <Route path='/' element={<Home/>}/>
-            <Route path='/cart' element={<Cart/>}/>
+            <Route path="/" element={<Home items={pizzas}/>} />
+            <Route path="/cart" element={<Cart />} />
           </Routes>
         </div>
       </div>
